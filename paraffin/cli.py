26f741be03f3ee6c7d45d69d7211c777c6b1a7b7
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-07-18 15:44:33 +0200

    visualize using dash

--- paraffin/cli.py
+++ paraffin/cli.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import subprocess
+import threading
 from concurrent.futures import Future, ProcessPoolExecutor, as_completed
 from typing import List, Optional
 
@@ -10,14 +11,21 @@ import dvc.stage
 import networkx as nx
 import typer
 
+
 log = logging.getLogger(__name__)
 
 app = typer.Typer()
 
+stages: List[dvc.stage.PipelineStage] = []
+finished: set[str] = set()
+submitted: set[Future] = set()
+graph: nx.DiGraph = nx.DiGraph()
+positions: dict = {}
+
 
 def run_stage(stage_name: str, max_retries: int) -> str:
     """Run the DVC repro command for a given stage and retry if an error occurs."""
-    command = ["dvc", "repro", "--single-item", stage_name]
+    command = ["dvc", "repro", "--single-item", stage_name, "--quiet"]
     for attempt in range(max_retries):
         log.debug(f"Attempting {stage_name}, attempt {attempt + 1} of {max_retries}...")
         process = subprocess.Popen(command, stderr=subprocess.PIPE, text=True)
@@ -64,30 +72,24 @@ def get_predecessor_subgraph(
     return subgraph
 
 
-@app.command()
-def main(
-    max_workers: Optional[int] = None,
-    targets: List[str] = typer.Argument(None),
-    max_retries: int = 3,
-):
-    if max_workers is None:
-        max_workers = os.cpu_count()
-        typer.echo(f"Using {max_workers} workers")
+def execute_graph(max_workers: int, targets: List[str], max_retries: int):
     with dvc.repo.Repo() as repo:
-        graph: nx.DiGraph = repo.index.graph
+        # graph: nx.DiGraph = repo.index.graph
+        # add to the existing graph
+        global graph
+        graph.add_nodes_from(repo.index.graph.nodes)
+        positions.update(nx.spring_layout(graph))
         # reverse the graph
         graph = graph.reverse()
         # construct a subgraph of the targets and their dependencies
         if targets:
-            stages = [stage for stage in graph.nodes if stage.addressing in targets]
-            graph = get_predecessor_subgraph(graph, stages)
+            selected_stages = [
+                stage for stage in graph.nodes if stage.addressing in targets
+            ]
+            graph = get_predecessor_subgraph(graph, selected_stages)
         log.debug("Graph:", graph)
-        stages: List[dvc.stage.PipelineStage] = list(
-            reversed(list(nx.topological_sort(graph)))
-        )
+        stages.extend(list(reversed(list(nx.topological_sort(graph)))))
 
-        finished: set[str] = set()
-        submitted: set[Future] = set()
         with ProcessPoolExecutor(max_workers=max_workers) as executor:
             while len(finished) < len(stages):
                 for stage in stages:
@@ -107,6 +109,30 @@ def main(
 
                 for future in as_completed(submitted):
                     finished.add(future.result())
+    print("Finished all stages!")
+
+
+@app.command()
+def main(
+    max_workers: Optional[int] = None,
+    targets: List[str] = typer.Argument(None),
+    max_retries: int = 3,
+    dashboard: bool = True,
+):
+    if max_workers is None:
+        max_workers = os.cpu_count()
+        typer.echo(f"Using {max_workers} workers")
+
+    if not dashboard:
+        execute_graph(max_workers, targets, max_retries)
+    else:
+        from .dashboard import app as dashboard_app
+
+        execution_thread = threading.Thread(
+            target=execute_graph, args=(max_workers, targets, max_retries)
+        )
+        execution_thread.start()
+        dashboard_app.run_server()
 
 
 if __name__ == "__main__":
