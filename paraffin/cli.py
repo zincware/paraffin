691a512f60e49f97acc851749857e083374f231c
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-07-18 14:20:44 +0200

    update workflow

--- paraffin/cli.py
+++ paraffin/cli.py
@@ -1,34 +1,71 @@
+import logging
+from concurrent.futures import Future, ProcessPoolExecutor, as_completed
+from typing import List
+
+import dvc.cli
+import dvc.repo
 import dvc.stage
 import networkx as nx
-import dvc.repo
-import dvc.cli
-from concurrent.futures import ProcessPoolExecutor, as_completed
-from typing import List
 import typer
-import logging
 
 log = logging.getLogger(__name__)
-# set log level to debug
-logging.basicConfig(level=logging.DEBUG)
 
 app = typer.Typer()
 
 
-def run_stage(stage_name: str):
-    log.debug(f"Running stage {stage_name}")
-    dvc.cli.main(["repro", stage_name, "--quiet"])
-    log.debug(f"Finished stage {stage_name}")
+def run_stage(stage_name: str) -> str:
+    # We use `single-item` for performance reasons
+    # all previous stages are already computed
+    dvc.cli.main(["repro", "--single-item", stage_name])
+    return stage_name
+
+
+def get_predecessor_subgraph(
+    graph: nx.DiGraph, nodes: List[dvc.stage.PipelineStage]
+) -> nx.DiGraph:
+    # Create an empty set to hold all nodes to be included in the subgraph
+    subgraph_nodes = set(nodes)
+
+    # Iterate over each node and add all its ancestors (predecessors) to the set
+    for node in nodes:
+        subgraph_nodes.update(nx.ancestors(graph, node))
+
+    # Create the subgraph with the collected nodes
+    subgraph = graph.subgraph(subgraph_nodes).copy()
+
+    return subgraph
 
 
 @app.command()
-def main(max_workers: int = 4):
+def main(max_workers: int = 4, targets: List[str] = typer.Argument(None)):
     with dvc.repo.Repo() as repo:
         graph: nx.DiGraph = repo.index.graph
-        stages: List[dvc.stage.PipelineStage] = list(nx.topological_sort(graph))
+        # reverse the graph
+        graph = graph.reverse()
+        # construct a subgraph of the targets and their dependencies
+        if targets:
+            stages = [stage for stage in graph.nodes if stage.addressing in targets]
+            graph = get_predecessor_subgraph(graph, stages)
+        log.debug("Graph:", graph)
+        stages: List[dvc.stage.PipelineStage] = list(
+            reversed(list(nx.topological_sort(graph)))
+        )
+
+        finished: set[str] = set()
+        submitted: set[Future] = set()
         with ProcessPoolExecutor(max_workers=max_workers) as executor:
-            futures = [executor.submit(run_stage, stage.addressing) for stage in stages]
-            for future in as_completed(futures):
-                future.result()
+            while len(finished) < len(stages):
+                for stage in stages:
+                    if stage.addressing in finished:
+                        continue
+                    if all(
+                        pred.addressing in finished
+                        for pred in graph.predecessors(stage)
+                    ):
+                        submitted.add(executor.submit(run_stage, stage.addressing))
+
+                for future in as_completed(submitted):
+                    finished.add(future.result())
 
 
 if __name__ == "__main__":
